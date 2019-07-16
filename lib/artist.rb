508725bc80a59d97ce68da846d6b36676010369c
Sarah Harrs <sarahharrs@gmail.com>
2019-07-16 23:03:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,7 +25,7 @@ def self.find_or_create_by_name(name)
   #(my.artist = self.all.find{|artist| artist.name==name} ? my_artist :self.new(name)
   self.all.find {|artist| artist.name==name} || self.new(name)
 end
-
+#/n means starts a new line
 
   def print_songs
     self.songs.each.do |song|
